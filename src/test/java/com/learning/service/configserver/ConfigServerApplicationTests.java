17acc65a278c9c6677e741033879bdf6b1e2cd79
Garima Kapoor <garima.kapoor@ibm.com>
2019-11-08 16:26:10 -0600

    added files

---
+++ src/test/java/com/learning/service/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.service.configserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
